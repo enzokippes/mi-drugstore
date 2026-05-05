83faa8142c1c6d29a69e1bff7be5dccd048aecf7
Enzo <enzov@example.com>
2026-05-05 17:07:52 -0300

    Fix prisma initialization with datasources

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL || "file:./dev.db",
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL || "file:./dev.db",
+    },
+  },
 });
 
 export default prisma;
